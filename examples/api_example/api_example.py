b79f6b39274f6d62b10164ce11908575d43e1b87
MRRollMan <konkow@ukr.net>
2020-12-28 17:55:56 +0200

    changed example

--- examples/api_example/api_example.py
+++ examples/api_example/api_example.py
@@ -4,8 +4,9 @@ from discordlib import API
 
 async def main():
     api = API("TOKEN")
-    me = await api.request("GET", "/users/@me")
-    print(me)
+    me = await api.resources.user.get_current()
+    print(f'{me.username}({me.id})')
+    await api.close()
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
